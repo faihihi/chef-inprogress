e4a5e35ee0806e960336520015fd18c93b1b95b1
Jinvara Vesvijak <jves0423@uni.sydney.edu.au>
2019-10-15 15:04:11 +1100

    Edited globalvariables

--- app/src/main/java/au/edu/sydney/comp5216/chef_inprogress/GlobalVariables.java
+++ app/src/main/java/au/edu/sydney/comp5216/chef_inprogress/GlobalVariables.java
@@ -1,38 +1,19 @@
 package au.edu.sydney.comp5216.chef_inprogress;
 
 import android.app.Application;
-import android.view.ActionMode;
 
 import au.edu.sydney.comp5216.chef_inprogress.ui.add.CategoryPagerAdapter;
-import au.edu.sydney.comp5216.chef_inprogress.ui.inventory.ViewPagerAdapter;
 
 public class GlobalVariables extends Application {
-    private ActionMode currentMode;
 
     CategoryPagerAdapter viewPagerAdapter;
-    private int currentCategory;
 
-    public ActionMode getCurrentMode() {
-        return currentMode;
-    }
 
     public CategoryPagerAdapter getViewPagerAdapter() {
         return viewPagerAdapter;
     }
 
-    public int getCurrentCategory() {
-        return currentCategory;
-    }
-
-    public void setCurrentMode(ActionMode currentMode) {
-        this.currentMode = currentMode;
-    }
-
     public void setViewPagerAdapter(CategoryPagerAdapter viewPagerAdapter) {
         this.viewPagerAdapter = viewPagerAdapter;
-    }
-
-    public void setCurrentCategory(int currentCategory) {
-        this.currentCategory = currentCategory;
     }
 }
